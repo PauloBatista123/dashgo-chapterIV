f11b4ad16920a4a4a39dccd7dc42e041f899211a
Paulo Henrique <paulo-henrique-morada@hotmail.com>
2022-07-30 16:15:49 -0300

    Componente paginacao

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,43 @@
+import { Box, Button, Stack } from "@chakra-ui/react";
+
+export function Pagination(){
+  return(
+    <Stack
+      direction={"row"}
+      mt="8"
+      justify={"space-between"}
+      align="center"
+      spacing={"6"}
+    >
+      <Box>
+        <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
+      </Box>
+      <Stack direction={"row"}>
+        <Button
+          size="sm"
+          fontSize={"xs"}
+          width="4"
+          colorScheme={"pink"}
+          bg="pink.500"
+          _disabled={{
+            bgColor: 'pink.500',
+            cursor: 'default'
+          }}>
+          1
+        </Button>
+        <Button
+          size="sm"
+          fontSize={"xs"}
+          width="4"
+          colorScheme={"pink"}
+          bg="gray.700"
+          _hover={{
+            bgColor:
+              'gray.500',
+          }}>
+          2
+        </Button>
+      </Stack>
+    </Stack>
+  );
+}
