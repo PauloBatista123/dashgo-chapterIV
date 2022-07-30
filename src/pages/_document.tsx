7ce021bfda645de57ff8240d6c4b688ee87f9ac1
Paulo Henrique <paulo-henrique-morada@hotmail.com>
2022-07-30 10:25:51 -0300

    Criando pagina de login

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,11 @@
-import Document, { Html, Main, NextScript } from "next/document";
-import Head from "next/head";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="pt-BR">
         <Head>
+          <meta charSet="utf-8" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Amatic+SC&family=Poppins:wght@400;600&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
